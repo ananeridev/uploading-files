6e43778bf6711eef719b295e6fa8201688f1603a
anabneri <ananeri32@hotmail.com>
2021-01-02 20:58:31 -0300

    feat(service): creating a service

--- initial/src/main/java/com/example/uploadingfiles/UploadingFilesApplication.java
+++ initial/src/main/java/com/example/uploadingfiles/UploadingFilesApplication.java
@@ -3,8 +3,10 @@ package com.example.uploadingfiles;
 import com.example.uploadingfiles.storage.StorageService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@SpringBootApplication
 public class UploadingFilesApplication
 {
     public static void main( String[] args )
---
+++ initial/src/main/java/com/example/uploadingfiles/storage/StorageProperties.java
@@ -0,0 +1,19 @@
+package com.example.uploadingfiles.storage;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("storage")
+public class StorageProperties {
+
+    private String location = "upload-dir";
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+
+}
